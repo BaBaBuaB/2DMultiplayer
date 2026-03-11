e75482858990799e08a7590afccfa641345768f1
BaBaBuaB <arm5052sueksa@gmail.com>
2026-03-12 03:12:03 +0700

    Fix complete

--- Assets/Scripts/Utils/DestroySelfOnContract.cs
+++ Assets/Scripts/Utils/DestroySelfOnContract.cs
@@ -6,7 +6,7 @@ public class DestroySelfOnContract : MonoBehaviour
     [SerializeField] private Projectile projectile;
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(projectile.TeamIndex != -1)
+        if(projectile.TeamIndex != -1 && col.gameObject.CompareTag("Player"))
         {
             if (col.TryGetComponent<TankPlayer>(out TankPlayer player))
             {
